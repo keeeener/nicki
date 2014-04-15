0390fc9f506c9e717a138f8f1577932bcd285bf0
keeeener <brandonkeener8@gmail.com>
2014-04-15 17:14:17 -0400

    Initial Source

---
+++ platform/external/bluetooth/bluedroid/hci/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        src/bt_hci_bdroid.c \
+        src/lpm.c \
+        src/bt_hw.c \
+        src/btsnoop.c \
+        src/utils.c
+
+ifeq ($(QCOM_BT_USE_SMD_TTY),true)
+
+LOCAL_CFLAGS += -DQCOM_WCN_SSR
+
+endif
+
+LOCAL_SRC_FILES += \
+        src/userial.c \
+        src/userial_mct.c \
+        src/hci_mct.c \
+        src/hci_h4.c\
+        src/hci_ibs.c
+
+LOCAL_C_INCLUDES += \
+        $(LOCAL_PATH)/include \
+        $(LOCAL_PATH)/../utils/include
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        liblog \
+        libdl \
+        libbt-utils
+
+LOCAL_MODULE := libbt-hci
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
